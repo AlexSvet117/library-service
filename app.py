426e65551b0c9e97053f840a7ed5fd1775c1431c
AlexSvet117 <alex.svet117@gmail.com>
2025-04-04 00:01:14 +0100

    get selected book

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-
+# list of books
 books = [
     {"book_id": "001", "title":"Pete the Cat and Cupcakes", "author": "Some Dude", "publication_year": 2005, 
      "genre":"Kids books", "read_status":"read", "rating": 5.0, "notes":"Great book for bed reading"},
@@ -20,6 +20,20 @@ books = [
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+# returns all the books from the list of books
+@app.route("/api/books", methods = ["GET"])
+def get_all_books():
+    return jsonify(books)
+
+# returns selected book
+@app.route("/api/books/<string:book_id>")
+def get_selected_book(book_id):
+    for book in books:
+        if book_id == book.get("book_id"):
+            return jsonify(book), 200
+    return jsonify({"error" :f"The book with book id {book_id} is not found"}), 404
 
 
 
